9f82f5cb3916fdb74c65493765391fb539db386d
s2lsoftener <s2@puyonexus.com>
2023-10-16 22:21:08 -0400

    .env fix

--- dev/init-env.sh
+++ dev/init-env.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 shopt -s globstar
 
-cp .env.example .env
+# cp .env.example .env
 
 for i in services/*/.env.example; do
     OUT_FILE=$(echo $i | sed -e 's/.env.example/.env/')
